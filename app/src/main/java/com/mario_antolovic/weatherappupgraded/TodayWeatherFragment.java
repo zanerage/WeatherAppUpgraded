9bd28cd5a1ab6446e007926b1d2168c6c92c4c99
zanerage <marioantolovic0@gmail.com>
2019-07-19 21:34:47 +0200

    Drawable Url pictures from openweather site Connecting text from layout

--- app/src/main/java/com/mario_antolovic/weatherappupgraded/TodayWeatherFragment.java
+++ app/src/main/java/com/mario_antolovic/weatherappupgraded/TodayWeatherFragment.java
@@ -2,6 +2,7 @@ package com.mario_antolovic.weatherappupgraded;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mario_antolovic.weatherappupgraded.Common.Common;
+import com.mario_antolovic.weatherappupgraded.Model.WeatherResult;
 import com.mario_antolovic.weatherappupgraded.Retrofit.IOpenWeatherMap;
 import com.mario_antolovic.weatherappupgraded.Retrofit.RetrofitClient;
+import com.squareup.picasso.Picasso;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
 
@@ -72,11 +80,56 @@ public class TodayWeatherFragment extends Fragment {
         txt_geo_coords = (TextView)itemView.findViewById(R.id.txt_get_coords);
         txt_sunrise = (TextView)itemView.findViewById(R.id.txt_sunrise);
         txt_sunset = (TextView)itemView.findViewById(R.id.txt_sunset);
+        txt_description = (TextView)itemView.findViewById(R.id.txt_description);
+        txt_pressure = (TextView)itemView.findViewById(R.id.txt_pressure);
+        txt_tempature = (TextView)itemView.findViewById(R.id.txt_tempature);
+        txt_date_time = (TextView)itemView.findViewById(R.id.txt_date_time);
+
+
+        weather_panel = (LinearLayout)itemView.findViewById(R.id.weathger_panel);
+        loading = (ProgressBar)itemView.findViewById(R.id.loading);
+
+
+        getWeatherInformation();
 
 
 
 
         return itemView;
+    }
+
+    private void getWeatherInformation() {
+
+        compositeDisposable.add(mService.getWeatherByLatLng(String.valueOf(Common.current_location),
+                String.valueOf(Common.current_location.getLongitude()),
+                Common.APP_ID,
+                "metric")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<WeatherResult>() {
+                    @Override
+                    public void accept(WeatherResult weatherResult) throws Exception {
+
+                        //load image
+                        Picasso.get().load(new StringBuilder("https://openweathermap.org/img/wn/01n@2x.png")
+                        .append(weatherResult.getWeather().get(0).getIcon())
+                        .append(".png").toString()).into(img_weather);
+
+                        txt_city_name.setText(weatherResult.getName());
+                        txt_description.setText(new StringBuilder("The current Weather in ")
+                        .append(weatherResult.getName()).toString());
+                        txt_tempature.setText(new StringBuilder(Sna));
+
+                    }
+
+                    },new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Toast.makeText(getActivity(), ""+throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+    })
+
+        );
     }
 
 }
