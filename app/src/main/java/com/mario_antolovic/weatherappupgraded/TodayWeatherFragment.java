a446cc33057cdad786902e4ef8cf056180fdb610
zanerage <marioantolovic0@gmail.com>
2019-07-19 21:17:47 +0200

    Fragment today weather Design v2 todayweatherfragment.java functions

--- app/src/main/java/com/mario_antolovic/weatherappupgraded/TodayWeatherFragment.java
+++ app/src/main/java/com/mario_antolovic/weatherappupgraded/TodayWeatherFragment.java
@@ -6,12 +6,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.mario_antolovic.weatherappupgraded.Retrofit.IOpenWeatherMap;
+import com.mario_antolovic.weatherappupgraded.Retrofit.RetrofitClient;
+
+import io.reactivex.disposables.CompositeDisposable;
+import retrofit2.Retrofit;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TodayWeatherFragment extends Fragment {
+
+
+    ImageView img_weather;
+    TextView txt_city_name,txt_humidity,txt_sunrise,txt_sunset,txt_pressure,txt_tempature,txt_description,txt_date_time,txt_wind,txt_geo_coords;
+    LinearLayout weather_panel;
+    ProgressBar loading;
+
+    CompositeDisposable compositeDisposable;
+    IOpenWeatherMap mService;
+
+
+
 
     static TodayWeatherFragment instance;
 
@@ -28,6 +50,11 @@ public class TodayWeatherFragment extends Fragment {
 
 
     public TodayWeatherFragment() {
+         compositeDisposable = new CompositeDisposable();
+        Retrofit retrofit = RetrofitClient.getInstance();
+        mService = retrofit.create(IOpenWeatherMap.class);
+
+
         // Required empty public constructor
     }
 
@@ -36,7 +63,20 @@ public class TodayWeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_weather, container, false);
+        View itemView =  inflater.inflate(R.layout.fragment_today_weather, container, false);
+
+        img_weather = (ImageView)itemView.findViewById(R.id.img_weather);
+        txt_city_name = (TextView)itemView.findViewById(R.id.txt_city_name);
+        txt_humidity = (TextView)itemView.findViewById(R.id.txt_humidity);
+        txt_wind = (TextView)itemView.findViewById(R.id.txt_wind);
+        txt_geo_coords = (TextView)itemView.findViewById(R.id.txt_get_coords);
+        txt_sunrise = (TextView)itemView.findViewById(R.id.txt_sunrise);
+        txt_sunset = (TextView)itemView.findViewById(R.id.txt_sunset);
+
+
+
+
+        return itemView;
     }
 
 }
